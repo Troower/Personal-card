0d4df666d90b409bef7ff9b62479d9bbdc0e1c6f
IP-2b <IP-2b@DESKTOP-7A06TF1>
2025-04-09 17:30:03 +0300

    GoHome

--- Authorization.cs
+++ Authorization.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using PersonalCard.Properties;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,7 +16,13 @@ namespace PersonalCard
     {
         public Authorization()
         {
+
             InitializeComponent();
+            string a = "";
+            foreach (string str in File.ReadAllLines(@"D:\IP-2b\Personal-card\config.ini")) a += str;
+            MessageBox.Show(a);
+           
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
