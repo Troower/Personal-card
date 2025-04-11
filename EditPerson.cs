1cfc054ca965d0cf37b58a385f83c95e87e1c4f1
IP-2b <IP-2b@DESKTOP-7A06TF1>
2025-04-11 16:27:41 +0300

    Go home

--- EditPerson.cs
+++ EditPerson.cs
@@ -6,9 +6,7 @@ using System.Threading.Tasks;
 
 namespace PersonalCard
 {
-     internal class Person
+    internal class EditPerson
     {
-        
     }
-    
 }
---
+++ LoadPerson.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalCard
+{
+    static internal class LoadPerson
+    {
+        public static Person getPerson()
+        {
+            Person person = new Person();
+
+            return person;
+        }
+    }
+}
